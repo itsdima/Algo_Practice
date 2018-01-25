9caa7dabcf78b2e2cf12965ae7ba12c58ab3709e
Eric Pricop <dimitripricop@yahoo.com>
2018-01-25 10:31:25 -0800

    added algo to reverse an array

--- algo.js
+++ algo.js
@@ -10,3 +10,15 @@ function DoubleUp(arr){
 	return arr; 
 }
 console.log(DoubleUp([7,4,10]))
+
+function reverse(arr){
+	var temp; 
+	for (var i = 0; i < arr.length/2; i++) {
+		temp = arr[i];
+		arr[i] = arr[arr.length-1-i]; 
+		arr[arr.length-1-i]=temp
+	}
+	return arr; 
+}
+
+console.log(reverse([2,4,6,8]))
